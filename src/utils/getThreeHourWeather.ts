fcbb06879d8bfe0f01df7e83ffd340ae3c87cd06
Serkan İşyapan <serkanisyapan1@hotmail.com>
2023-02-21 19:31:51 +0300

    fix: type issue

--- src/utils/getThreeHourWeather.ts
+++ src/utils/getThreeHourWeather.ts
@@ -1,4 +1,6 @@
-export const getThreeHourWeather = (array) => {
+import { HourlyEntity } from "@/types/WeatherTypes";
+
+export const getThreeHourWeather = (array: Array<HourlyEntity>) => {
   let newHourlyArr = [];
   for (let i = 0; i < 22; i += 3) {
     newHourlyArr.push(array[i]);
