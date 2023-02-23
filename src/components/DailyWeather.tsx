e64f8395f0e73e20f403c5ca7204d6cb510f65ca
Serkan İşyapan <serkanisyapan1@hotmail.com>
2023-02-23 13:55:55 +0300

    fix:alignment issue on daily weathers tab

--- src/components/DailyWeather.tsx
+++ src/components/DailyWeather.tsx
@@ -3,11 +3,15 @@ import { getWeatherDay } from "@/utils/getCurrentDate";
 
 export const DailyWeather = ({ dailyWeather }: DailyWeatherTypes) => {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-8 mt-[60px] gap-2">
+    <div className="grid grid-cols-3 md:grid-cols-8 mt-[60px]">
       {dailyWeather.map((daily, dailyID) => (
-        <div className="text-white text-center" key={dailyID}>
+        <div
+          className="text-white flex flex-col justify-center items-center mb-3"
+          key={dailyID}
+        >
           <span>{getWeatherDay(daily.dt)}</span>
           <img
+            className="w-[100px] h-[100]"
             src={`https://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`}
           />
           <span>
